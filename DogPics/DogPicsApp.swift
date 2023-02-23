ebdabfe953f126eafbffe9264c5e0387a1833081
Philip Keller <philipjoelkeller@gmail.com>
2023-02-23 10:02:09 -0800

    Initial Commit

---
+++ DogPics/DogPicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogPicsApp.swift
+//  DogPics
+//
+//  Created by Philip Keller on 2/23/23.
+//
+
+import SwiftUI
+
+@main
+struct DogPicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
