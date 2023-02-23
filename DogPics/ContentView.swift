846413026b820c94b1530efe6f9e391a8b3adda3
Philip Keller <philipjoelkeller@gmail.com>
2023-02-23 10:40:08 -0800

    Successful API call of random image printed to the console - YouTube MidTerm Solution (Dog Pics App)

--- DogPics/ContentView.swift
+++ DogPics/ContentView.swift
@@ -8,12 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    enum Breed: String, CaseIterable {
+        case boxer, bulldog, chihuahua, corgi, labradoodle, poodle, pug, retriever
+    }
+    
+    @StateObject var dogVM = DogViewModel()
+    @State private var selectedBreed: Breed = .boxer
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("🐶 Dog Pics!")
+                .font(Font.custom("Avenir Next Condensed", size: 60))
+                .bold().foregroundColor(.brown)
+                .lineLimit(1)
+                .minimumScaleFactor(0.5)
+            
+            Spacer()
+            
+            Button("Any Random Dog") {
+                Task {
+                    await dogVM.getData()
+                }
+                
+            }
+            .buttonStyle(.borderedProminent)
+            .bold()
+            .tint(.brown)
+            .padding(.bottom)
+            
+            HStack {
+                Button("Show Breed") {
+                    
+                }
+                .buttonStyle(.borderedProminent)
+                .padding(.bottom)
+                
+                Picker("", selection: $selectedBreed) {
+                    ForEach(Breed.allCases, id: \.self) { breed in
+                        Text(breed.rawValue.capitalized)
+                            
+                    }
+                }
+            }
+            .bold()
+            .tint(.brown)
         }
         .padding()
     }
---
+++ DogPics/DogViewModel.swift
@@ -0,0 +1,42 @@
+//
+//  DogViewModel.swift
+//  DogPics
+//
+//  Created by Philip Keller on 2/23/23.
+//
+
+import Foundation
+
+@MainActor
+
+class DogViewModel: ObservableObject {
+    struct Result: Codable {
+        var message: String
+    }
+    
+    @Published var imageURL = ""
+    var urlString = "https://dog.ceo/api/breeds/image/random"
+    
+    func getData() async {
+        print("🕸️ We are accessing the url \(urlString)")
+        
+        // Convert the urlString to the specific URL type
+        guard let url = URL(string: urlString) else {
+            print("😡 ERROR: Could not create a URL from \(urlString)")
+            return
+        }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            
+            guard let result = try? JSONDecoder().decode(Result.self, from: data) else {
+                print("😡 JSON ERROR: Could not decode returned JSON data from \(urlString)")
+                return
+            }
+            imageURL = result.message
+            print("The imageURL is: \(imageURL)")
+        } catch {
+            print("😡 ERROR: Could not use URL at \(urlString) to get data & response.")
+        }
+    }
+}
